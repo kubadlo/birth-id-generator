912e4a852112166622e191331dc5731204314e3f
Jakub Leško <mail@jakublesko.com>
2023-03-02 20:42:05 +0100

    Use gender as enum and simplify string formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use chrono::prelude::*;
 use rand::prelude::*;
 
+#[derive(Debug)]
+enum Gender {
+    Male,
+    Female,
+}
+
 fn is_leap_year(year: &u16) -> bool {
     year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
@@ -31,33 +37,25 @@ fn generate_day(generator: &mut ThreadRng, year: &u16, month: &u8) -> u8 {
     generator.gen_range(1..=last_day_of_month(year, month))
 }
 
-fn generate_gender(generator: &mut ThreadRng) -> u8 {
-    generator.gen_range(0..=1)
+fn generate_gender(generator: &mut ThreadRng) -> Gender {
+    match generator.gen_range(0..=1) {
+        1 => Gender::Female,
+        _ => Gender::Male,
+    }
 }
 
 fn generate_seq(generator: &mut ThreadRng) -> u16 {
     generator.gen_range(0..=999)
 }
 
-fn combine_date_parts(year: &u16, month: &u8, day: &u8, gender: &u8) -> String {
+fn combine_date_parts(year: &u16, month: &u8, day: &u8, gender: &Gender) -> String {
     let year_short: u8 = (year % 100) as u8;
     let month_with_gender: u8 = match *gender {
-        1 => *month + 50,
+        Gender::Female => *month + 50,
         _ => *month,
     };
 
-    let compare_fmt = |condition_result, value| -> String {
-        match condition_result {
-            true => format!("0{}", value),
-            false => format!("{}", value),
-        }
-    };
-
-    let year_fmt: String = compare_fmt(year_short < 10, year_short);
-    let month_fmt: String = compare_fmt(month_with_gender < 10, month_with_gender);
-    let day_fmt: String = compare_fmt(*day < 10, *day);
-
-    format!("{}{}{}", year_fmt, month_fmt, day_fmt)
+    format!("{:0>2}{:0>2}{:0>2}", year_short, month_with_gender, *day)
 }
 
 fn create_birth_id(date_part: &String, seq_part: &u16) -> String {
@@ -69,13 +67,7 @@ fn create_birth_id(date_part: &String, seq_part: &u16) -> String {
         _ => control_modulo,
     };
 
-    let seq_fmt = match seq_part {
-        0..=9 => format!("00{}", seq_part),
-        10..=99 => format!("0{}", seq_part),
-        _ => format!("{}", seq_part),
-    };
-
-    format!("{}/{}{}", date_part, seq_fmt, control_number)
+    format!("{}/{:0>3}{}", date_part, seq_part, control_number)
 }
 
 fn main() {
@@ -84,7 +76,7 @@ fn main() {
     let year: u16 = generate_year(&mut random_generator);
     let month: u8 = generate_month(&mut random_generator);
     let day: u8 = generate_day(&mut random_generator, &year, &month);
-    let gender: u8 = generate_gender(&mut random_generator);
+    let gender: Gender = generate_gender(&mut random_generator);
     let seq: u16 = generate_seq(&mut random_generator);
 
     let date_part: String = combine_date_parts(&year, &month, &day, &gender);
@@ -93,13 +85,6 @@ fn main() {
     println!("Year:    {}", year);
     println!("Month:   {}", month);
     println!("Day:     {}", day);
-    println!(
-        "Gender:  {}",
-        match gender {
-            1 => "Female",
-            _ => "Male",
-        }
-    );
-
+    println!("Gender:  {:?}", gender);
     println!("BirthId: {}", birth_id);
 }
